6aec548243db0333b0ad454a83418d601c2a5edf
msteyha <murha.a.o.-ik52@edu.kpi.ua>
2026-03-02 11:43:30 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+      
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string path = "C:\\Users\\o.murha\\source\\repos\\лабораторна 1 (2 семестр)\\лабораторна 1 (2 семестр)\\TextFile1.txt"; 
+
+        List<int> symbolCounts = new List<int>();
+
+        
+        string[] lines = File.ReadAllLines(path);
+
+        foreach (string line in lines)
+        {
+            symbolCounts.Add(line.Length);
+        }
+
+        for (int i = 0; i < symbolCounts.Count; i++)
+        {
+            Console.WriteLine($"Рядок {i + 1}: {symbolCounts[i]} символів");
+        }
+    }
+}
